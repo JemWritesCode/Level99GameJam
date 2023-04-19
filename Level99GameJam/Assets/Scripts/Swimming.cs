c75b3ae815931787d911d9f65ed481c4dfb5dfc8
Jem Cope <thejemmeh@gmail.com>
2023-04-18 20:22:08 -0700

    Basic Swimming Controls
    Swimming type movement while in the water but natural land movement when you leave the water.

--- Level99GameJam/Assets/Scripts/Swimming.cs
+++ Level99GameJam/Assets/Scripts/Swimming.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Crest;
+using System;
 
 public class Swimming : MonoBehaviour
 {
 
     [SerializeField] AudioSource underWaterSound;
-    // Start is called before the first frame update
+    public float swimSpeed = 1f;
+    public Transform target;
+
+    bool isBelowWater;
+
+    Rigidbody rigidBody;
+
     void Start()
     {
         GameObject ocean = GameObject.Find("Ocean");
         OceanRenderer oceanRenderer = ocean.GetComponent<OceanRenderer>();
+        rigidBody = GetComponent<Rigidbody>();   
+    }
 
-        
+    private void FixedUpdate()
+    {
+         if (isBelowWater)
+        {
+            SwimMovement();
+        }
     }
 
 
@@ -21,12 +35,42 @@ public class Swimming : MonoBehaviour
     {
         Debug.Log("I'm underwater");
         underWaterSound.Play();
+        SwimMovement();
+        isBelowWater = true;
+
+    }
+
+    private void SwimMovement()
+    {
+        rigidBody.useGravity = false;
+        if(Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.position += target.forward * swimSpeed * Time.deltaTime;
+        }
+        if(Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.position -= target.forward * swimSpeed * Time.deltaTime;
+        }
+        ResetVelocity();
     }
 
     public void isNotUnderwater()
     {
         Debug.Log("I'm NOT underwater");
         underWaterSound.Stop();
+        StopSwimMovement();
+        isBelowWater = false;
+        ResetVelocity();
     }
 
+    private void StopSwimMovement()
+    {
+        rigidBody.useGravity = true;
+    }
+
+    public void ResetVelocity()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+    }
 }
