ed565140b37f2271f5d5c6445f30c6dab2054e79
redseiko <redseiko@users.noreply.github.com>
2023-04-20 23:16:39 -0700

    Add label to ItemList panel and call it "Loot".
    Preparation for adding an ItemGrid panel below it called "Upgrades" or something like that.
    
    Add CoinsShinyEffect for TreasuryUI when setting coins value.

--- Level99GameJam/Assets/Scripts/UI/Controllers/TreasuryUIController.cs
+++ Level99GameJam/Assets/Scripts/UI/Controllers/TreasuryUIController.cs
@@ -1,3 +1,5 @@
+using Coffee.UIEffects;
+
 using DG.Tweening;
 
 using UnityEngine;
@@ -16,6 +18,9 @@ public class TreasuryUIController : MonoBehaviour {
   [field: SerializeField]
   public Image CoinsImage { get; private set; }
 
+  [field: SerializeField, Header("Effect")]
+  public UIShiny CoinsShinyEffect { get; private set; }
+
   int _currentCoinsValue = 0;
 
   public void ResetPanel() {
@@ -25,6 +30,8 @@ public class TreasuryUIController : MonoBehaviour {
 
   public void SetCoinsValue(int coinsValue) {
     CoinsValue.DOCounter(_currentCoinsValue, coinsValue, 0.25f, false);
+    CoinsShinyEffect.Play();
+
     _currentCoinsValue = coinsValue;
   }
 }
