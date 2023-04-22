21f294a306755a64029a9d453c682539abeeb81d
Jem Cope <thejemmeh@gmail.com>
2023-04-22 15:23:20 -0700

    rock shakes and moves down while making aound

--- Level99GameJam/Assets/Scripts/TreasuryDoor.cs
+++ Level99GameJam/Assets/Scripts/TreasuryDoor.cs
@@ -5,69 +5,45 @@ using DG.Tweening;
 
 public class TreasuryDoor : MonoBehaviour
 {
+    Transform parentObjectTransform;
     Vector3 doorOriginalPosition;
     Vector3 doorMoveToCoords;
     Vector3 doorMoveBy;
-
-    Transform parentObjectTransform;
-    Vector3 shakeStrength;
-    Sequence shakeAndGoDown;
+    [SerializeField] float Amplitude = .5f; // this becomes how far it can move I think
     
     [SerializeField] AudioSource doorMoveSound;
-
-    [SerializeField] float Amplitude = 1f; // this becomes how far it can move I think
-    [SerializeField] float Duration = .1f;
-  
 
     public void moveTreasuryDoor()
     {
         Debug.Log("You interacted with the sword on the Treasury Door!");
 
-        parentObjectTransform = gameObject.transform.parent.gameObject.transform;
+        shakeAndGoDown();
+        doorMoveSound.Play();
 
+        // Leaving this here in case I wanna try to spurce up the shake more
+        //shake, then go down, shake, then go down
+        //shakeAndGoDown = DOTween.Sequence()
+        //    .Insert(0f, parentObjectTransform.DOMove(doorMoveToCoords, 2f, false))
+        //    .Insert(0f,parentObjectTransform.DOShakePosition(2f, shakeStrength, 2, 5, false, true, ShakeRandomnessMode.Harmonic));
+    }
+
+    private void shakeAndGoDown()
+    {
+        parentObjectTransform = gameObject.transform.parent.gameObject.transform;
         doorOriginalPosition = parentObjectTransform.position;
         doorMoveToCoords.Set(239.110001f, 176.809998f, 469.691589f);
         doorMoveBy = doorMoveToCoords - doorOriginalPosition;
 
-
-        //shakeStrength.Set(0f, 0f, .25f);
-
         var randomTremor = GetRandomTremor();
 
-        parentObjectTransform.DOBlendableMoveBy(randomTremor, Duration)
-            .OnUpdate(() => randomTremor = GetRandomTremor())
-            .SetLoops(4, LoopType.Yoyo)
-            .SetEase(Ease.InFlash);
-        //parentObjectTransform.DOBlendableMoveBy(doorMoveBy, 3f).SetLoops(4, LoopType.Incremental);
-
-
-
-        //var randomTremor = GetRandomTremor();
-        //shakeAndGoDown = DOTween.Sequence()
-        //    .Insert(parentObjectTransform.DOBlendableMoveBy(randomTremor, Duration))
-        //    .Insert(0f, parentObjectTransform.DOBlendableMoveBy(doorMoveBy, 2f))
-        //    //.OnUpdate(() => randomTremor = GetRandomTremor())
-        //    .SetLoops(4, LoopType.Incremental)
-        //    .SetEase(Ease.InFlash);
-
-        //shake, then go down, shake, then go down
-
-
-
-        //shakeAndGoDown = DOTween.Sequence()
-        //    .Insert(0f, parentObjectTransform.DOMove(doorMoveToCoords, 2f, false))
-        //    .Insert(0f,parentObjectTransform.DOShakePosition(2f, shakeStrength, 2, 5, false, true, ShakeRandomnessMode.Harmonic));
-
-
-
-        // Do blendable move by
-        // do punch
-        //maybe can use a sequence instead of blendable
+        parentObjectTransform.DOBlendableMoveBy(randomTremor, .5f)
+            .SetLoops(6, LoopType.Yoyo);
+        parentObjectTransform.DOBlendableMoveBy(doorMoveBy, 5f);
     }
 
     private Vector3 GetRandomTremor()
     {
-        return new Vector3(Random.Range(0, Amplitude), Random.Range(0, Amplitude), Random.Range(0, Amplitude));
+        return new Vector3(Random.Range(.2f, Amplitude), 0, Random.Range(.2f, Amplitude));
     }
 
 }
