572d38315add9b0314e623bde02379a2bfbc2b60
redseiko <redseiko@gmail.com>
2023-04-21 23:18:11 -0700

    Add an ItemInfoSplash image when no ItemSlot is selected.

--- Level99GameJam/Assets/Scripts/Extensions/UnityExtensions.cs
+++ Level99GameJam/Assets/Scripts/Extensions/UnityExtensions.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class UnityExtensions {
   public static RectTransform RectTransform(this Component component) {
     return (RectTransform) component.transform;
+  }
+
+  public static Image SetAlpha(this Image image, float alpha) {
+    Color color = image.color;
+    color.a = alpha;
+    image.color = color;
+
+    return image;
   }
 }
--- Level99GameJam/Assets/Scripts/UI/Controllers/ItemInfoUIController.cs
+++ Level99GameJam/Assets/Scripts/UI/Controllers/ItemInfoUIController.cs
@@ -1,12 +1,16 @@
 using DG.Tweening;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemInfoUIController : MonoBehaviour {
   [field: SerializeField]
   public CanvasGroup ItemInfoPanel { get; private set; }
 
   [field: SerializeField]
+  public Image ItemInfoSplash { get; private set; }
+
+  [field: SerializeField, Header("Item")]
   public TMPro.TMP_Text ItemName { get; private set; }
 
   [field: SerializeField]
@@ -21,10 +25,11 @@ public class ItemInfoUIController : MonoBehaviour {
   public void ResetPanel() {
     ItemInfoPanel.alpha = 0f;
     ItemInfoPanel.blocksRaycasts = false;
+    ItemInfoSplash.SetAlpha(1f);
   }
 
   public void SetPanel(string name, string description, bool showBadge) {
-    DOTween.Complete(ItemInfoPanel, withCallbacks: true);
+    ItemInfoPanel.DOComplete(true);
 
     DOTween.Sequence()
         .SetTarget(ItemInfoPanel)
@@ -38,10 +43,16 @@ public class ItemInfoUIController : MonoBehaviour {
         .Insert(0f, ItemDescription.transform.DOPunchPosition(new(0f, 7.5f, 0f), 0.3f, 0, 0))
         .Insert(0f, ItemBadge.transform.DOPunchPosition(new(0f, 7.5f, 0f), 0.3f, 0, 0))
         .Insert(0.15f, ItemInfoPanel.DOFade(1f, 0.15f));
+
+    ItemInfoSplash.DOComplete(true);
+    ItemInfoSplash.DOFade(0f, 0.15f);
   }
 
   public void HidePanel() {
-    ItemInfoPanel.DOKill();
+    ItemInfoPanel.DOComplete(true);
     ItemInfoPanel.DOFade(0f, 0.25f);
+
+    ItemInfoSplash.DOComplete(true);
+    ItemInfoSplash.DOFade(1f, 0.25f);
   }
 }
