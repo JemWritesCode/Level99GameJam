cd14dfa74cd24fd0fde19c474afa1bbac52a5513
Jem Cope <thejemmeh@gmail.com>
2023-04-23 11:45:52 -0700

    working on adding fish collectables to sell

---
+++ Level99GameJam/Assets/Scripts/TakeSteelDagger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeSteelDagger : MonoBehaviour
+{
+    // remove dagger and riddle from scene
+    // add the Captain's Riddle to your inventory. It can be the item description so that they can reread it.
+    // Add Captain's Knife to Inventory (This becomes the key to the treasury)
+    // paper and knife out of wood sound
+}
