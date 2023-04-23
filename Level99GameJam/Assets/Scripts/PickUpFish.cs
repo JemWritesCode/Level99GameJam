91ba092fd3a8b6e38ac7dbec7234c45dad135107
Jem Cope <thejemmeh@gmail.com>
2023-04-23 16:28:16 -0700

    Added outline to interactable items so they're easier to see.

---
+++ Level99GameJam/Assets/Scripts/PickUpFish.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpFish : MonoBehaviour
+{
+    [SerializeField] InventoryItemData fishType;
+
+    public void grabTheFishie()
+    {
+        // jem todo look into singletons so you don't have to cache in every script every times
+
+        InventoryManager.Instance.addToInventory(fishType);
+        GameObject.Destroy(this.gameObject);
+    }
+
+}
