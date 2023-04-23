35a80eef2791344a8a98e5e0cd803dc76c1d593b
Jem Cope <thejemmeh@gmail.com>
2023-04-23 00:13:37 -0700

    Added the Captain's Key to the cargo hold

---
+++ Level99GameJam/Assets/InteractWithKey.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithKey : MonoBehaviour
+{
+    // make a key pickup sound
+    // delete the key from the scene
+    // add the key to the player's inventory
+}
