1397e20e19f845a9414ac10e37e50b531e73db09
Jem Cope <thejemmeh@gmail.com>
2023-04-23 19:33:07 -0700

    clean up the fish manager script

--- Level99GameJam/Assets/Scripts/FishManager.cs
+++ Level99GameJam/Assets/Scripts/FishManager.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class FishManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> fishPrefabs; // The GameObject to spawn.
-    //public float minDistance = 1000f; // The minimum distance between fish.
-    //public float maxDistance = 1000f; // The maximum distance between fish.
-    //public float topOfOceanApprox = 180f;
-    //public float bottomOfOceanApprox = 100f;
+    [SerializeField] List<GameObject> fishPrefabs; 
 
     [SerializeField] int fishPoolSize = 30;
     GameObject[] fishPool;
 
-    public float RateOfSpawn = 1;
-
-    private float nextSpawn = 0;
-
-    // Update is called once per frame
     void Start()
     {
         fishPool = new GameObject[fishPoolSize];
@@ -34,18 +25,6 @@ public class FishManager : MonoBehaviour
    
         
     }
-
-    //private void PopulateFishPool()
-    //{
-    //    fishPool = new GameObject[fishPoolSize];
-
-    //    for (int i = 0; i < fishPool.Length; i++)
-    //    {
-    //        Vector3 randomPosition = transform.position + Random.insideUnitSphere * 100f;
-    //        GameObject newFish = Instantiate(RandomFishPrefab(), randomPosition, transform.rotation);
-    //        fishPool[i] = newFish;
-    //    }
-    //}
 
     private GameObject RandomFishPrefab()
     {
