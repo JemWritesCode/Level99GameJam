5dfb9db010cfda28c6b9444f3c7108cec54964f4
redseiko <redseiko@gmail.com>
2023-04-25 20:18:55 -0700

    Modify LadderMove to be more laddery. Add durations for climb and move. Apply to one ladder right at start.

--- Level99GameJam/Assets/Scripts/Game/LadderMove.cs
+++ Level99GameJam/Assets/Scripts/Game/LadderMove.cs
@@ -6,9 +6,17 @@ public class LadderMove : MonoBehaviour {
   [field: SerializeField]
   public Vector3 MoveToPosition { get; private set; }
 
+  [field: SerializeField, Min(0f)]
+  public float ClimbDuration { get; private set; } = 1f;
+
+  [field: SerializeField, Min(0f)]
+  public float MoveDuration { get; private set; } = 0.5f;
+
+  Collider _ladderCollider;
   Collider _playerCollider;
 
   private void Start() {
+    _ladderCollider = GetComponent<Collider>();
     _playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();
   }
 
@@ -24,10 +32,13 @@ public class LadderMove : MonoBehaviour {
     _isMovingPlayer = true;
     _playerCollider.enabled = false;
 
+    Vector3 ladderPosition = _ladderCollider.ClosestPointOnBounds(_playerCollider.transform.position);
+
     DOTween.Sequence()
-        .Insert(0f, _playerCollider.transform.DOMove(MoveToPosition, 1f))
-        .InsertCallback(
-            1f,
+        .Insert(0f, _playerCollider.transform.DOMove(ladderPosition, 0.5f).SetEase(Ease.Linear))
+        .Append(_playerCollider.transform.DOMoveY(MoveToPosition.y, ClimbDuration).SetEase(Ease.Linear))
+        .Append(_playerCollider.transform.DOMove(MoveToPosition, MoveDuration).SetEase(Ease.Linear))
+        .AppendCallback(
             () => {
               _isMovingPlayer = false;
               _playerCollider.enabled = true;
